a747a16fa2dbf42e8c7ae39d4e8c65fb6565c64a
Konstantin0 <maschkovc@yandex.ru>
2021-12-27 14:54:16 +0300

    перенос на JUnit

--- src/test/java/HelpdeskUITest.java
+++ src/test/java/HelpdeskUITest.java
@@ -1,25 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 import pages.AbstractPage;
 import pages.LoginPage;
 
 import java.io.IOException;
-import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class HelpdeskUITest {
 
     private WebDriver driver;
     String nameTicket = "Уникальный билет 345";
 
-    @BeforeClass
+    @Before
     public void setup() throws IOException {
         System.getProperties().load(ClassLoader.getSystemResourceAsStream("config.properties"));
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         AbstractPage.setDriver(driver);
     }
 
